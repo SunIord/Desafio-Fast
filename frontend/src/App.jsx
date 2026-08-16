2d73c28b28d423687b7a05400032a844140d2e2d
jpam <jpam@cin.ufpe.br>
2026-08-15 21:35:28 -0300

    feat(frontend): adiciona rota /participacao e link no nav

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import ColaboradoresPage from "./pages/ColaboradoresPage";
 import WorkshopsPage from "./pages/WorkshopsPage";
 import WorkshopDetailPage from "./pages/WorkshopDetailPage";
+import ParticipacaoPage from "./pages/ParticipacaoPage";
 
 function App() {
   return (
@@ -9,12 +10,14 @@ function App() {
       <nav>
         <Link to="/colaboradores">Colaboradores</Link>
         <Link to="/workshops">Workshops</Link>
+        <Link to="/participacao">Participação</Link>
       </nav>
 
       <Routes>
         <Route path="/colaboradores" element={<ColaboradoresPage />} />
         <Route path="/workshops" element={<WorkshopsPage />} />
         <Route path="/workshops/:id" element={<WorkshopDetailPage />} />
+        <Route path="/participacao" element={<ParticipacaoPage />} />
         <Route path="*" element={<ColaboradoresPage />} />
       </Routes>
     </BrowserRouter>
